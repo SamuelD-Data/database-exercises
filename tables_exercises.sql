e5ff27d78074f1835605223ff1f0b2afa1cdafdb
Samuel Davila <samuelrdavila@gmail.com>
2020-07-20 14:20:39 -0500

    Tables exercise completed

---
+++ tables_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+show tables;
+
+/*Data types in the employees table includes date, int, enum, and varchar.
+I expect at least one numeric column to appear in the salaries table.
+
+I expect at least one string column to appear in the employees table.
+
+I expect at least one date column to appear in the employees table.
+
+The departments table and the employees table are connected via the dept_emp table where 
+each employee number is associated with a dept number that denotes which dept that are in.*/
+
+SHOW create table dept_manager;
