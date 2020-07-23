f5fa419fecbd66fee85e0eb78b58efe2448ac912
Samuel Davila <samuelrdavila@gmail.com>
2020-07-23 13:43:17 -0500

    Completed indexes exercises

---
+++ indexes_exercises.sql
@@ -0,0 +1,19 @@
+-- USE your employees database. --
+
+USE employees;
+
+-- DESCRIBE each table and inspect the keys and see which columns have indexes and keys. --
+
+DESC departments;
+
+DESC dept_emp;
+
+DESC dept_manager;
+
+DESC employees;
+
+DESC employees_with_departments;
+
+DESC salaries;
+
+DESC titles;
