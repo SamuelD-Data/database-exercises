d2c6fe06729523c9358aaf00f083de8f287ce09d
Samuel Davila <samuelrdavila@gmail.com>
2020-07-28 17:08:31 -0500

    updated question 1

--- case_exercises.sql
+++ case_exercises.sql
@@ -1,8 +1,20 @@
--- Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not. --
+-- Write a query that returns all employees (emp_no), their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
 
-select E.emp_no, dept_no, hire_date, to_date, IF(to_date > curdate(), true, false) as is_current_employee
-from employees as E
-join dept_emp as DE on DE.emp_no = E.emp_no;
+SELECT emp.emp_no, d.dept_no, emp.start_date, emp.end_date, emp.is_current_emp
+FROM dept_emp d 
+JOIN 
+(SELECT dept_emp.emp_no # dept.dept_no
+		, max(employees.hire_date) AS start_date
+		, max(dept_emp.to_date) AS end_date
+		, max(CASE WHEN dept_emp.to_date > curdate() THEN 1 ELSE 0 END) 
+			AS is_current_emp
+FROM dept_emp
+JOIN employees USING(emp_no)
+GROUP BY dept_emp.emp_no #, dept.dept_no
+) emp
+ON d.emp_no = emp.emp_no AND d.to_date = emp.end_date
+;
+
 
 -- Write a query that returns all employee names, and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name. --
 
@@ -10,19 +22,21 @@ select last_name,
 	CASE
 		WHEN last_name < 'I' THEN 'A-H'
 		WHEN last_name < 'R' THEN 'I-Q'
-		ELSE 'R-Z'
+		WHEN last_name > 'Q' THEN 'R-Z'
+		ELSE null
 		END AS alpha_group
 from employees;
 
 -- How many employees were born in each decade? -- 
 
-select birth_date,
+select count(birth_date),
 	CASE
 		WHEN substr(birth_date, 1,4) < '1960' then '1950s'
 		WHEN substr(birth_date, 1,4) < '1970' then '1960s'
-		else 'not 50s or 60s'
+		else null
 		end as birth_decade
-from employees;
+from employees
+GROUP BY birth_decade;
 
 -- BONUS --
 
@@ -43,4 +57,3 @@ join dept_emp as DE ON DE.emp_no = S.emp_no
 join departments as D on D.dept_no = DE.dept_no
 group by department_groups
 ;
-
